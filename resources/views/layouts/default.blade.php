dacf0346cd01871b38045fac350dae4f4d7167fb
z1ming <402832626@qq.com>
2019-07-23 08:56:59 +0000

    Finish layout and routes

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,17 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>@yield('title','Weibo App') - Laravel</title>
+  <title>@yield('title','Sample App') - Laravel</title>
+  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
 </head>
 <body>
-  @yield('content')
+  @include('layouts._header')
+
+  <div class="container">
+      @yield('content')
+      @include('layouts._footer')
+  </div>
+
+
 </body>
 </html>
