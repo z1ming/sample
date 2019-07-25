00b9e98a85b68273b0d3ad8fa0e9029d270b71ca
z1ming <402832626@qq.com>
2019-07-25 07:09:36 +0000

    Finish login and logout

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -3,7 +3,7 @@
     <small class="slogon">
       <img class="brand-icon" src="https://cdn.learnku.com/uploads/images/201612/12/1/iq7WQc2iuW.png?imageView2/1/w/34/h/34" />
     <a href="#">
-      刻意练习,每日精进
+      你不要担心
     </a>
     </small>
     <nav>
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,18 +1,20 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title>@yield('title','Sample App') - Laravel</title>
-  <link rel="stylesheet" type="text/css" href="{{ mix('css/app.css') }}">
+  <title>@yield('title', 'Sample App') - Ziming Sample</title>
+  <link rel="stylesheet" href="/css/app.css">
 </head>
 <body>
   @include('layouts._header')
 
   <div class="container">
+    <div class="col-md-offset-1 col-md-10">
       @include('shared._messages')
       @yield('content')
       @include('layouts._footer')
+    </div>
   </div>
 
-
+  <script src="/js/app.js"></script>
 </body>
 </html>
