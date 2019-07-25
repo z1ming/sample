c1f9797d305385d32768efd5610ab4a25b5ad659
z1ming <402832626@qq.com>
2019-07-25 03:38:51 +0000

    Updated database configuration

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,7 @@
 
 require __DIR__.'/helpers.php';
 
+
 $app = new Illuminate\Foundation\Application(
     realpath(__DIR__.'/../')
 );
--- bootstrap/helpers.php
+++ bootstrap/helpers.php
@@ -4,7 +4,7 @@ function get_db_config()
 {
     if(getenv('IS_IN_HEROKU'))
     {
-        $url = parse_url(getenv("DATABESE_URL"));
+        $url = parse_url(getenv("DATABASE_URL"));
 
         return $db_config = [
             'connection' => 'pgsql',
@@ -17,7 +17,7 @@ function get_db_config()
         return $db_config = [
             'connection' => env('DB_CONNECTION','mysql'),
             'host'       => env('DB_HOST','localhost'),
-            'database'   => env('DB_DATABESE','forge'),
+            'database'   => env('DB_DATABASE','forge'),
             'username'   => env('DB_USERNAME','forge'),
             'password'   => env('DB_PASSWORD',''),
         ];
